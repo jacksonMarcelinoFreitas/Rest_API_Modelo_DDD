fde93379dfc2e43896b4fabdc3faa38394c1fc0d
Jackson Marcelino de Freitas <jacksonzitap.mc@gmail.com>
2023-05-31 10:37:04 -0300

    some updates

--- RestApiModeloDDD.Domain/Entitys/Cliente.cs
+++ RestApiModeloDDD.Domain/Entitys/Cliente.cs
@@ -5,13 +5,13 @@ namespace RestApiModeloDDD.Domain.Entitys
     public class Cliente : Base
     {
         public string Nome { get; set; }
+
         public string Sobrenome { get; set; }
 
         public string Email { get; set; }
 
         public DateTime DataCadastro { get; set; }
 
-        public bool IsAtivo{ get; set; }
-
+        public bool IsAtivo { get; set; }
     }
 }
